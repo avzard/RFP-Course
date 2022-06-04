763c08fc2d7133a94be3285c0cfc40c87a90c0c0
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-04 18:32:59 +0530

    First commit

---
+++ EmployeeWagee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//USING IF ELSE LOOPS 
+namespace BasicProgram
+{
+    internal class EmployeeWagee
+    {
+
+        public int EmpPresent = 2;
+        public int Wge_Per_Hour = 20;
+        public int Full_Day_Hour = 8;
+        public int Partime_hour = 4;
+        public int Full_Time = 1;
+        public int Daily_wage = 0;
+
+        // public int DaiyWage { get; private set; }
+
+        // checking functionality===method 
+
+        public void CheckEmpPresentAbsent()
+        {
+            Random EmpCheck = new Random();
+            int Value = EmpCheck.Next(0, 2);
+            if (Value == EmpPresent)
+            {
+                Random TimeCheck = new Random();
+                int WorkingHours = TimeCheck.Next(0, 2);
+                if (WorkingHours == Full_Time)
+                {
+                    Daily_wage = Wge_Per_Hour * Full_Day_Hour;
+                    Console.WriteLine("Employee is Present  " + Daily_wage);
+                }
+                else
+                {
+                    Daily_wage = Wge_Per_Hour * Partime_hour;
+                    Console.WriteLine("Employee is Present for PartTime" + Daily_wage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using basicprogram;
+using BasicProgram;
+using System;
+namespace BaicProgram
+{
+    class program
+    {
+
+        public static void Main(String[] args)
+        {
+
+            /*EmployeeWagee Emp = new EmployeeWagee();
+            Emp.CalculateWage();*/
+
+            /* EmployeeWage Emp = new EmployeeWage();
+             Emp.CalculateWage();*/
+
+            EmployeeWage06 Emp = new EmployeeWage06();
+            Emp.CalucalteWage();
+
+        }
+    }
+
+}
